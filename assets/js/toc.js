81009962b0b8cbdf455af92dab787e85c848390b
Gaurav <lotusheart2016@gmail.com>
2024-05-27 05:14:59 +0100

    objective page done. Also worked on creating responsive navigation and rest of the page. wrote toc.css and toc.js file whihc provide function to create a responsive page and also a interaxctive button. Next step is to add text animations.

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,4 +1,7 @@
 const navBar = document.querySelector('.nav-bar');
+const modules = document.querySelector('.modules-dropdown-contents');
+const modulesButton = document.querySelector('.modules');
+
 function wrapDiv() {
 	// Check if the screen width is less than 600px
 	if (window.innerWidth < 600) {
@@ -23,7 +26,7 @@ function wrapDiv() {
 		// If the screen is resized to more than 600px, remove the menuDiv if it exists
 		const existingMenuDiv = document.querySelector('.menu');
 		if (existingMenuDiv) {
-			const navBar = existingMenuDiv.querySelector('.nav-bar');
+			// Reuse the original navBar variable instead of redefining it
 			existingMenuDiv.parentNode.insertBefore(navBar, existingMenuDiv);
 			existingMenuDiv.remove();
 			navBar.classList.remove('hide-element');
@@ -36,3 +39,21 @@ window.addEventListener('load', wrapDiv);
 
 // Call wrapDiv when the window is resized
 window.addEventListener('resize', wrapDiv);
+
+// Ensure dropBtn is only added if it exists
+window.addEventListener('load', () => {
+	const dropBtn = document.querySelector('.dropbtn');
+	if (dropBtn) {
+		dropBtn.addEventListener('click', () => {
+			navBar.classList.toggle('hide-element');
+			navBar.classList.toggle('shrink-navbar');
+			navBar.classList.toggle('nav-bar');
+			dropBtn.classList.toggle('inactive-menu-button');
+			modules.classList.add('hide-element');
+		});
+	}
+});
+
+modulesButton.addEventListener('click', () => {
+	modules.classList.toggle('hide-element');
+});
